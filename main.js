dcc33680000b423585e249df8e0cd602f2cd6000
brianluu06 <42229175+brianluu06@users.noreply.github.com>
2018-08-30 19:19:57 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+
+var cardThree = "king"
+var cardFour = "king"
+var cardOne = "queen";
+var cardTwo = "queen"
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
